1a3f38964e9632968a51be3cbde9e64ae7e1e39b
Chris <he1237596@163.com>
2025-04-28 15:27:45 +0800

    feat: 更新eslint配置

--- eslint.config.js
+++ eslint.config.js
@@ -2,47 +2,59 @@ const { defineConfig } = require('eslint-define-config');
 const eslintPluginReact = require('eslint-plugin-react');
 const eslintPluginPrettier = require('eslint-plugin-prettier');
 const eslintPluginTypescript = require('@typescript-eslint/eslint-plugin');
-const typescriptEslintParser = require('@typescript-eslint/parser');
 
 module.exports = defineConfig({
-  // 使用 flat config 的配置方式
-  // languageOptions: {
-  //   parser: typescriptEslintParser, // 设置自定义的 TypeScript 解析器为对象
-  //   parserOptions: {
-  //     ecmaVersion: 2020,
-  //     sourceType: 'module',
-  //     ecmaFeatures: {
-  //       jsx: true, // 支持 JSX 语法
-  //     },
+  // defineConfig导致使用 flat config 的配置方式无效
+  languageOptions: {
+    parser: require('@typescript-eslint/parser'), // 设置自定义的 TypeScript 解析器为对象
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true, // 支持 JSX 语法
+      },
+      // project: ['./tsconfig.json'], // 不建议加 unless 你要用更严格的规则
+    },
+  },
+  // parser: '@typescript-eslint/parser',
+  // parserOptions: {
+  //   ecmaVersion: 2020,
+  //   sourceType: 'module',
+  //   ecmaFeatures: {
+  //     jsx: true,
   //   },
+  //   // project: ['./tsconfig.json'], // 不建议加 unless 你要用更严格的规则
   // },
-  // plugins: {
-  //   react: eslintPluginReact,
-  //   prettier: eslintPluginPrettier,
-  //   '@typescript-eslint': eslintPluginTypescript,
-  // },
-  // files: ['src/**/*.{js,ts,tsx}'], // 需要 lint 的文件路径
-  // rules: {
-  //   'prettier/prettier': [
-  //     'warn',
-  //     {
-  //       semi: false, // 可以设置为 true 或 false
-  //       singleQuote: false, // 允许使用单引号和双引号
-  //       trailingComma: 'none', // 允许对象结尾有逗号
-  //       endOfLine: 'auto',
-  //     },
-  //   ],
-  //   'semi': ['off'], // 不强制要求分号
-  //   '@typescript-eslint/semi': ['off'], // 不检查 TypeScript 中的分号
-  //   'quotes': ['off', 'single', { 'avoidEscape': true }], // 允许单双引号
-  //   '@typescript-eslint/quotes': ['off'],
-  //   '@typescript-eslint/no-unused-vars': ['off'], // 启用未使用变量检查
-  //   'react/react-in-jsx-scope': 'off',
-  // },
-  // settings: {
-  //   react: {
-  //     version: 'detect', // 自动检测 React 版本
-  //   },
-  // },
-  // ignores: ['node_modules', 'build'], // 忽略的目录或文件
+  plugins: {
+    react: require('eslint-plugin-react'),
+    prettier: require('eslint-plugin-prettier'),
+    '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+  },
+
+  files: ['src/**/*.{js,ts,tsx}'], // 需要 lint 的文件路径
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        semi: true, // 允许结尾分号
+        singleQuote: true, // 允许使用单引号
+        trailingComma: 'all', // all: 允许对象结尾有逗号 none: 不允许
+        endOfLine: 'auto',
+      },
+    ],
+    semi: ['off'],
+    '@typescript-eslint/semi': 'off',
+    quotes: 'off',
+    '@typescript-eslint/quotes': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn', // 未使用变量警告
+    '@typescript-eslint/no-explicit-any': 'off', // 想用 any 的话可以关掉
+    'react/react-in-jsx-scope': 'off', // React 17+可以不用 import React
+    // 'quotes': ['off', 'single', { 'avoidEscape': true }], // 允许单双引号
+  },
+  settings: {
+    react: {
+      version: 'detect', // 自动检测 React 版本
+    },
+  },
+  ignores: ['node_modules/', 'build/', 'dist/', 'public/'], // 忽略的目录或文件
 });
